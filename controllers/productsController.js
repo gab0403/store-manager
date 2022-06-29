d5e6305ae57ab3f9844f2086ab60488a255e96df
Gabriela <gabriela0403rodriguesdossanto@gmail.com>
2022-06-29 20:05:07 -0300

    Requisito 1 tentando arrumar erro

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -9,7 +9,7 @@ const getProducts = async (_req, res) => {
 const getProductsById = async (req, res) => {
     const { id } = req.params;
     const productsResultById = await productsService.getProductsById(id);
-    if (!productsResultById || productsResultById.length === 0) {
+    if (!productsResultById || productsResultById < 1) {
       return res.status(404).json({ message: 'Product not found' });
     }
   return res.status(200).json(productsResultById[0]);
