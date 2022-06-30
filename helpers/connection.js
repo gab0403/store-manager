754baaf5d746d346c0d75705f00613347fd84fb1
Gabriela <gabriela0403rodriguesdossanto@gmail.com>
2022-06-30 14:37:49 -0300

    Arruma connection

--- helpers/connection.js
+++ helpers/connection.js
@@ -1,11 +1,12 @@
 const mysql = require('mysql2/promise');
+require('dotenv').config();
 
 const connection = mysql.createPool({
-  host: process.env.MYSQL_HOST || 'localhost',
-  user: 'root',
-  port: 3306,
-  database: 'StoreManager',
-  password: '1234',
+  host: 'localhost' || process.env.MYSQL_HOST,
+  user: 'root' || process.env.MYSQL_USER,
+  port: 3306 || process.env.PORT,
+  database: 'StoreManager' || process.env.MYSQL_DATABASE,
+  password: '1234' || process.env.MYSQL_PASSWORD,
 });
 
 module.exports = connection;
